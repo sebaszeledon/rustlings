c05815b4a07e6909400a1896c61e523e494d1aac
sebaszeledon <sebas.zeledon@gmail.com>
2024-08-15 13:00:50 -0600

    primitives completed

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,12 @@
 fn main() {
     // You can optionally experiment here.
+    let a = [1, 2, 3, 4, 5];
+
+        // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
+        // let nice_slice = ???
+
+    let nice_slice = &a[1..4];
+    print!("{:#?}", nice_slice);
 }
 
 #[cfg(test)]
@@ -10,6 +17,9 @@ mod tests {
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
         // let nice_slice = ???
+
+        let nice_slice = &a[1..4];
+        print!("{:#?}", nice_slice);
 
         assert_eq!([2, 3, 4], nice_slice);
     }
